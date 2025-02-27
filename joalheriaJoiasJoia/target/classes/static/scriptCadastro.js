03e91bc682c0e733384f07b2ed52837d4ac6e58a
Khallizi02 <159180451+Khallizi02@users.noreply.github.com>
2025-02-27 12:36:25 -0300

    Add files via upload

---
+++ joalheriaJoiasJoia/target/classes/static/scriptCadastro.js
@@ -0,0 +1,43 @@
+document.addEvenListener("DOMContentLoaded", () =>{
+
+  const form = document.getElementById("cadastroClienteForm");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+
+  const nomeCliente = document.getElementById("nomeCliente").value;
+  const cpf = document.getElementById("cpf").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const dataNascimento = document.getElementById("dataNascimento").value;
+
+  try {
+
+      const response = await fetch ("http://localhost:8080/cadrastoCliente",{
+ method: "POST",
+ headers: {
+     "Content-Type": "application/json"
+   },
+
+   body: JSON.stringify({
+    nomeCliente, 
+    cpf, 
+    email, 
+    telefone,
+    dataNascimento
+    }),
+});
+
+if (response.ok){
+ alert("Cliente cadastrado!");
+} else{
+ alert("Erro ao cadastrar!");
+}
+
+} catch (error) {
+  console.error("Erro ao cadastrar o cliente", error);
+}
+
+  });
+});
